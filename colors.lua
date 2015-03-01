8ebd5b5b5c93b06b106530f8a25d90d4266809a5
David Rohrbaugh <rohrbaugh.david17@libertycommon.org>
2015-02-28 21:01:24 -0700

    Update colors.lua

--- colors.lua
+++ colors.lua
@@ -1,5 +1,5 @@
 function redPrint(s)
-   print("\x1b[0;31m" .. s .. "\x1b[0m")
+   print(red(s))
 end
 
 function red(s)
@@ -7,7 +7,7 @@ function red(s)
 end
 
 function greenPrint(s)
-   print("\x1b[0;32m" .. s .. "\x1b[0m")
+   print(green(s))
 end
 
 function green(s)
@@ -15,43 +15,60 @@ function green(s)
 end
 
 function yellowPrint(s)
-   print("\x1b[0;33m" .. s .. "\x1b[0m")
+   print(yellow(s))
 end
 
 function yellow(s)
    return "\x1b[0;33m" .. s .. "\x1b[0m"
 end
 
+function bluePrint(s)
+   print(blue(s))
+end
+
+function blue(s)
+   return "\x1b[0;34m" .. s .. "\x1b[0m"
+end
+
+function purplePrint(s)
+   print(purple(s))
+end
+
+function purple(s)
+   return "\x1b[0;35m" .. s .. "\x1b[0m"
+end
+
+function lightBluePrint(s)
+   print(lightBlue(s))
+end
+
+function lightBlue(s)
+   return "\x1b[0;36m" .. s .. "\x1b[0m"
+end
+
 function colorPrint(s, c)
-   if c == "red" then
-      redPrint(s)
-   end
-   if c == "green" then
-      greenPrint(s)
-   end
-   if c == "yellow" then
-      yellowPrint(s)
-   end
+   print(colorText(s, c))
 end
 
 function colorText(s, c)
-   if c == "red" then
+   if c == "red" or c == "Red" then
       return red(s)
    end
-   if c == "green" then
+   if c == "green" or c == "Green" then
       return green(s)
    end
-   if c == "yellow" then
+   if c == "yellow" or c == "Yellow" then
       return yellow(s)
+   end
+   if c == "blue" or c == "Blue" then
+      return blue(s)
+   end
+   if c == "purple" or c == "Purple" then
+      return purple(s)
+   end
+   if c == "lightBlue" or c == "lightblue" or c == "Lightblue" or c == "LightBlue" then
+      return lightBlue(s)
    end
 end
 
---[[redPrint("<RED>")
-greenPrint("<GREEN>")
-yellowPrint("<YELLOW>")
-print(red("<RED>") .. green("<GREEN>") .. yellow("<YELLOW>"))
-colorPrint("<RED>", "red")
-colorPrint("<GREEN>", "green")
-colorPrint("<YELLOW>", "yellow")
-print(colorText("<RED>", "red") .. colorText("<GREEN>", "green") .. colorText("<YELLOW>", "yellow"))]]
-print("\x1b[0;34m<color>\x1b[0m")
+print(red("C") .. yellow("O") .. green("L") .. blue("O") .. purple("R") .. lightBlue("S"))

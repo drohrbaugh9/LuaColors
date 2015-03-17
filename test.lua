c5f15353e977ccc39350d19000e28a645d64040b
David Rohrbaugh <rohrbaugh.david17@libertycommon.org>
2015-03-16 20:01:55 -0600

    Update test.lua

--- test.lua
+++ test.lua
@@ -1,10 +1,6 @@
 c = require("colors")
 c.initialize()
-
-print(c.red("C") .. c.green("O") ..c.yellow("L") .. c.blue("O") .. c.purple("R") .. c.cyan("S"))
-
-print(c.multipleColors({{"C","red"},{"O","green"},{"L","yellow"},{"O","blue"},{"R","purple"},{"S","cyan"}}, ""))
-
 print(c.getAvailableColors())
-
+print(c.red("C") .. c.green("O") ..c.yellow("L") .. c.blue("O") .. c.purple("R") .. c.cyan("S"))
+print(c.multipleColors({{"C","red"},{"O","green"},{"L","yellow"},{"O","blue"},{"R","purple"},{"S","cyan"}}, ""))
 --c.terminalColor("yellOw")

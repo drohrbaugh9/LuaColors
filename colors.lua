332fadd0a2e779faab436304d68ec94fde6ad10c
David Rohrbaugh <rohrbaugh.david17@libertycommon.org>
2015-03-17 08:21:08 -0600

    Update colors.lua, test.lua

--- colors.lua
+++ colors.lua
@@ -1,103 +1,143 @@
 colorsLibrary = {
 
-_RED = "\x1b[0;31m", _GREEN = "\x1b[0;32m", _YELLOW = "\x1b[0;33m", _BLUE = "\x1b[0;34m", _PURPLE = "\x1b[0;35m", _CYAN = "\x1b[0;36m", _WHITE = "\x1b[0;37m", _COLOREND = "\x1b[0m",
+  _COLORSTART = "\x1b[",
+  _BLACKBACK = "40;",
+  _REDBACK = "41;",
+  _GREENBACK = "42;",
+  _YELLOWBACK = "43;",
+  _BLUEBACK = "44;",
+  _PURPLEBACK = "45;",
+  _CYANBACK = "46;",
+  _WHITEBACK = "47;",
+  _BLACKTEXT = "30m",
+  _REDTEXT = "31m",
+  _GREENTEXT = "32m",
+  _YELLOWTEXT = "33m",
+  _BLUETEXT = "34m",
+  _PURPLETEXT = "35m",
+  _CYANTEXT = "36m",
+  _WHITETEXT = "37m",
+  _COLOREND = "\x1b[0m",
 
-colors = {}, availableColors = "black, red, green, yellow, blue, purple, cyan, white",
+  colors = {},
 
-getAvailableColors = function()
-   return "The available colors are \x1b[0;47;30mblack\x1b[0m " .. colorsLibrary.red("red") .. ", " .. colorsLibrary.green("green") .. ", " .. colorsLibrary.yellow("yellow") .. ", " .. colorsLibrary.blue("blue") .. ", " .. colorsLibrary.purple("purple") .. ", " .. colorsLibrary.cyan("cyan") .. ", and white."
-end,
+  availableColors = "blacktext redtext greentext yellowtext bluetext purpletext cyantext whitetext blackback redback greenback yellowback blueback purpleback cyanback whiteback",
 
-values = function(t)
-  local i = 0
-  return function ()  i = i + 1; return t[i] end
-end,
+  getAvailableColors = function()
+    return "The available colors are \x1b[0;47;30mblack\x1b[0m " .. colorsLibrary.red("red") .. ", " .. colorsLibrary.green("green") .. ", " .. colorsLibrary.yellow("yellow") .. ", " .. colorsLibrary.blue("blue") .. ", " .. colorsLibrary.purple("purple") .. ", " .. colorsLibrary.cyan("cyan") .. ", and white."
+  end,
 
-initialize = function()
-   local c = {}
-   c["red"] = colorsLibrary._RED
-   c["green"] = colorsLibrary._GREEN
-   c["yellow"] = colorsLibrary._YELLOW
-   c["blue"] = colorsLibrary._BLUE
-   c["purple"] = colorsLibrary._PURPLE
-   c["cyan"] = colorsLibrary._CYAN
-   colorsLibrary.colors = c
-end,
+  values = function(t)
+    local i = 0
+    return function () i = i + 1; return t[i] end
+  end,
 
---function redPrint(s) print(red(s)) end
---function greenPrint(s) print(green(s)) end
---function yellowPrint(s) print(yellow(s)) end
---function bluePrint(s) print(blue(s)) end
---function purplePrint(s) print(purple(s)) end
---function lightBluePrint(s) print(lightBlue(s)) end
---function colorPrint(s, c) print(color(s, c)) end
---function multipleColorsPrint(t, c) print(multipleColors(t, c)) end
+  initialize = function()
+    local c = {}
+    c["blackback"] = colorsLibrary._BLACKBACK
+    c["redback"] = colorsLibrary._REDBACK
+    c["greenback"] = colorsLibrary._GREENBACK
+    c["yellowback"] = colorsLibrary._YELLOWBACK
+    c["blueback"] = colorsLibrary._BLUEBACK
+    c["purpleback"] = colorsLibrary._PURPLEBACK
+    c["cyanback"] = colorsLibrary._CYANBACK
+    c["whiteback"] = colorsLibrary._WHITEBACK
+    c["blacktext"] = colorsLibrary._BLACKTEXT
+    c["redtext"] = colorsLibrary._REDTEXT
+    c["greentext"] = colorsLibrary._GREENTEXT
+    c["yellowtext"] = colorsLibrary._YELLOWTEXT
+    c["bluetext"] = colorsLibrary._BLUETEXT
+    c["purpletext"] = colorsLibrary._PURPLETEXT
+    c["cyantext"] = colorsLibrary._CYANTEXT
+    c["whitetext"] = colorsLibrary._WHITETEXT
+    colorsLibrary.colors = c
+  end,
 
-color = function(s, c)
-   if not type(c) == "string" then
+  color = function(s, c, bc)
+
+    if not type(c) == "string" then
       error("string expected, got " .. type(c))
-   end
-   c = string.lower(c)
-   if string.find(colorsLibrary.availableColors, c) == nil then
-      if type(s) == "string" then return s end
-      error("string expected, got " .. type(s))
-   end
-   if type(s) == "string" or type(s) == "number" then
-      return colorsLibrary.colors[c] .. s .. colorsLibrary._COLOREND
-   elseif type(s) == "table" then
+    end
+
+    c = string.lower(c)
+    local textColor = ""
+
+    if not type(bc) == "string" then
+      error("string expected, got " .. type(bc))
+    end
+
+    bc = string.lower(bc)
+    local backColor = ""
+
+    if not string.find(colorsLibrary.availableColors, c) == nil then
+      textColor = colorsLibrary.colors[c]
+    else
+      textColor = "0m"
+    end
+
+    if not string.find(colorsLibrary.availableColors, bc) == nil then
+      backColor = colorsLibrary.colors[bc]
+    else
+      backColor = "0;"
+    end
+
+    if type(s) == "string" or type(s) == "number" then
+      return colorsLibrary._COLORSTART .. backColor .. textColor .. s .. colorsLibrary._COLOREND
+    elseif type(s) == "table" then
       local st = ""
       for v in colorsLibrary.values(t) do
-	 st = st .. colorsLibrary.colors[c] .. v .. colorsLibrary._COLOREND .. " "
+        st = st .. colorsLibrary._COLORSTART .. backColor .. textColor .. v .. colorsLibrary._COLOREND .. " "
       end
       return st
-   else
-      return colorsLibrary.colors[c] .. tostring(s) .. colorLibrary._COLOREND
-   end
-   error("string expected, got " .. type(s))
-end,
+    else
+      return colorsLibrary._COLORSTART .. backColor .. textColor .. tostring(s) .. colorLibrary._COLOREND
+    end
+    error("string expected, got " .. type(s))
+  end,
 
-red = function(s) return colorsLibrary.color(s, "red") end,
+  red = function(s) return colorsLibrary.color(s, "redtext", "") end,
 
-green = function(s) return colorsLibrary.color(s, "green") end,
+  green = function(s) return colorsLibrary.color(s, "greentext", "") end,
 
-yellow = function(s) return colorsLibrary.color(s, "yellow") end,
+  yellow = function(s) return colorsLibrary.color(s, "yellowtext", "") end,
 
-blue = function(s) return colorsLibrary.color(s, "blue") end,
+  blue = function(s) return colorsLibrary.color(s, "bluetext", "") end,
 
-purple = function(s) return colorsLibrary.color(s, "purple") end,
+  purple = function(s) return colorsLibrary.color(s, "purpletext", "") end,
 
-cyan = function(s) return colorsLibrary.color(s, "cyan") end,
+  cyan = function(s) return colorsLibrary.color(s, "cyantext", "") end,
 
-multipleColors = function(t, c)
-   if c == nil then
+  multipleColors = function(t, c)
+    if c == nil then
       c = " "
-   end
-   if type(t) == "table" then
+    end
+    if type(t) == "table" then
       if type(c) == "string" then
-	 local st = ""
-	 for s in colorsLibrary.values(t) do
-	    st = st .. colorsLibrary.color(s[1],s[2]) .. c
-	 end
-	 return st
+        local st = ""
+        for s in colorsLibrary.values(t) do
+          st = st .. colorsLibrary.color(s[1],s[2],s[3]) .. c
+        end
+        return st
       end
       error("string expected, got " .. type(c))
-   end
-   error("table expected, got " .. type(t))
-end,
+    end
+    error("table expected, got " .. type(t))
+  end--,
 
-terminalColor = function(c)
-   if type(c) == "string" then
+  --[[
+  terminalColor = function(c)
+    if type(c) == "string" then
       c = string.lower(c)
-      if c == "red" then io.write(colorsLibrary._RED) end
-      if c == "green" then io.write(colorsLibrary._GREEN) end
-      if c == "yellow" then io.write(colorsLibrary._YELLOW) end
-      if c == "blue" then io.write(colorsLibrary._BLUE) end
-      if c == "purple" then io.write(colorsLibrary._PURPLE) end
-      if c == "cyan" then io.write(colorsLibrary._CYAN) end
+      if c == "red" then io.write("\x1b[0;31m") end
+      if c == "green" then io.write("\x1b[0;32m") end
+      if c == "yellow" then io.write("\x1b[0;33") end
+      if c == "blue" then io.write("\x1b[0;34m") end
+      if c == "purple" then io.write("\x1b[0;35m") end
+      if c == "cyan" then io.write("\x1b[0;36m") end
       return
-   end
-   error("string expected, got " .. type(c))
-end
+    end
+    error("string expected, got " .. type(c))
+  end--]]
 }
 
 return colorsLibrary

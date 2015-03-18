40f3e1ca8bf1340b4dfa8794ff2c9adc24fed699
David Rohrbaugh <rohrbaugh.david17@libertycommon.org>
2015-03-17 18:05:24 -0600

    Update art.lua

--- art.lua
+++ art.lua
@@ -1,9 +1,9 @@
-c = require("colors") c.initialize()
-b = "blue"
-bt = {b,b,b,b,b,b,b,b,b,b,b,b,b,b,b,b,b,b,b,b,b,b,b,b}
-r = "red"
-w = "white"
-y = "yellow"
+local c = require("colors") c.initialize()
+local b = "blue"
+local bt = {b,b,b,b,b,b,b,b,b,b,b,b,b,b,b,b,b,b,b,b,b,b,b,b}
+local r = "red"
+local w = "white"
+local y = "yellow"
 io.write(c.colorMatrix({bt,bt,bt,
       {b,b,b,b,b,b,b,
         r,r,r,r,r,r,
@@ -11,12 +11,33 @@ io.write(c.colorMatrix({bt,bt,bt,
       {b,b,b,b,b,b,
         r,r,r,r,r,r,r,r,
         b,b,b,b,b,b,b,b,b,b},
-      
-    }))
---[[
-{w,w,w,w,w,
+      {b,b,b,b,b,
+        r,r,r,r,r,r,r,r,r,r,
+        b,b,b,b,b,b,b,b,b},
+      {w,w,w,w,w,
         r,r,r,
         y,y,y,y,
         r,r,r,
         w,w,w,w,w,w,w,w,w},
-      }))]]
+      {w,w,w,w,
+        r,r,r,
+        y,y,y,y,y,y,
+        w,w,w,w,w,w,w,w,w,w,w},
+      {w,w,w,w,
+        r,r,r,
+        y,y,y,y,y,y,
+        w,w,w,w,w,w,w,w,w,w,w},
+      {w,w,w,w,
+        r,r,r,
+        y,y,y,y,y,y,
+        w,w,w,w,w,w,w,w,w,w,w},
+      {w,w,w,w,
+        r,r,r,
+        y,y,y,y,y,y,
+        w,w,w,w,w,w,w,w,w,w,w},
+      {w,w,w,w,w,
+        r,r,r,
+        y,y,y,y,
+        r,r,r,
+        w,w,w,w,w,w,w,w,w},
+    }))
